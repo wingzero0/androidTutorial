52d655c0eb29747587a470cccdf150056b1c36d0
wingzero <wingzzz2003@hotmail.com>
2017-11-12 19:34:41 +0800

    init with empty activity

---
+++ app/src/main/java/com/example/kit/androidtutorial/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kit.androidtutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
